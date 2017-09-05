6d45da153f06094f29dede155188df5c6531b8c7
TheXeniL <xenilcool@gmail.com>
2017-09-05 11:49:49 +0700

    Initial Commit

---
+++ sib-soft task/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sib-soft task
+//
+//  Created by Никита Елизаров on 09/05/2017.
+//  Copyright © 2017 Никита Елизаров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
