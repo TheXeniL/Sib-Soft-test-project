b4b199fde8da67b5f97ee468af1824d1dda9a084
TheXeniL <xenilcool@gmail.com>
2017-09-11 23:26:45 +0700

    First commit

---
+++ sib-soft task/ImageDetailTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ImageDetailTableViewCell.swift
+//  sib-soft task
+//
+//  Created by Никита Елизаров on 09/07/2017.
+//  Copyright © 2017 Никита Елизаров. All rights reserved.
+//
+
+import UIKit
+
+class ImageDetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
